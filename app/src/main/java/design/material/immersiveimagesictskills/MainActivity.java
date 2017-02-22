ab7298fcfe1d7c912fa1f0fccaf190608d3c04f2
John Fitzgerald <usplitu@hotmail>
2017-02-22 16:58:37 +0000

    2 x 1 grid radio buttons using butterknife

--- app/src/main/java/design/material/immersiveimagesictskills/MainActivity.java
+++ app/src/main/java/design/material/immersiveimagesictskills/MainActivity.java
@@ -7,71 +7,63 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.RadioButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener
-{
-  ImageView imageView;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-  RadioButton centerBtn;
-  RadioButton fitEndBtn;
+public class MainActivity extends AppCompatActivity
+{
+
+  @BindView(R.id.imageView) ImageView imageView;
+
+  @BindView(R.id.centerBtn) RadioButton centerBtn;
+  @BindView(R.id.fitEndBtn) RadioButton fitEndBtn;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    imageView = (ImageView) findViewById(R.id.imageView);
-
-    centerBtn = (RadioButton) findViewById(R.id.centerBtn);
-    fitEndBtn = (RadioButton) findViewById(R.id.fitEndBtn);
-
-    centerBtn.setOnClickListener(this);
-    fitEndBtn.setOnClickListener(this);
+    ButterKnife.bind(this);
 
     centerBtn.setChecked(true);
-
-    imageView.setOnClickListener(this);
 
   }
 
   /**
-   * If radio button, not that in focus, is pressed
+   * If radio button, not the currently clicked, is pressed
    * then return that button view, else null.
-   *
-   * @param view The most recently clicked radio button.
-   * @return The clicked radio button if different to view param.
+   * @param view The currently clicked radio button
+   * @return The clicked radio button, if not the currently selected.
    */
-  public RadioButton getSelectedRadio(View view) {
-    RadioButton[] btns = {centerBtn, fitEndBtn};
-    for (RadioButton radioButton : btns) {
-      if (radioButton.isChecked() && radioButton != view) {
-        return radioButton;
+    public RadioButton getSelectedRadio(View view) {
+      RadioButton [] btns = {centerBtn, fitEndBtn};
+      for (RadioButton radioButton : btns) {
+        if (radioButton.isChecked() && radioButton != view) {
+          return radioButton;
+        }
       }
+      return null;
     }
-    return null;
-  }
 
-  @Override
-  public void onClick(View view) {
+  @OnClick({R.id.centerBtn, R.id.fitEndBtn})
+  public void clickRadioButton(RadioButton view) {
 
-    // Determine if a radio button clicked and implement handler
-    RadioButton checkedRadio = view instanceof RadioButton ? getSelectedRadio(view) : null;
-    if (checkedRadio != null) {
-      // Untick all buttons except that in focus.
-      if (checkedRadio != view) {
-        checkedRadio.setChecked(false);
-      }
-      String radioId = ((RadioButton) view).getText().toString();
-      switch (radioId) {
-        case "CENTER":
-          imageView.setScaleType(ImageView.ScaleType.CENTER);
-          break;
-        case "FIT_END":
+    // Check to see what is clicked
+    RadioButton checkedRadio = getSelectedRadio(view);
+    if (checkedRadio != null && checkedRadio != view) {
+      checkedRadio.setChecked(false);
+    }
+
+    switch(view.getText().toString()) {
+      case "CENTER":
+        imageView.setScaleType(ImageView.ScaleType.CENTER);
+        break;
+      case "FIT_END":
           imageView.setScaleType(ImageView.ScaleType.FIT_END);
-          break;
-      }
-    }
-    else {
-      // Other view handlers, such as clicking on the ImageView
-    }
+        break;
+     }
+
+
   }
 }
